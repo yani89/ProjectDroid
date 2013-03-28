b687517f37ab78d920b9e39910047ef0c8155bac
Harddisk-Yanipra <Harddisk-Yanipra@Harddisk-Yanipr>
2013-03-28 07:54:37 +0700

    Update sedikit dan error drpada gak diupdate hehehe..

--- ProjectDroid/src/com/projectdroid/app/OptionsActivity.java
+++ ProjectDroid/src/com/projectdroid/app/OptionsActivity.java
@@ -2,11 +2,34 @@ package com.projectdroid.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class OptionsActivity extends Activity {
-    public void onCreate(Bundle savedInstanceState) {
+   
+	private Spinner spinner1;
+	private Button btnSubmit;
+	
+	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settinglayout);
+        
+        addListenerOnSpineerItemSelection();
     }
+
+	private void addListenerOnSpineerItemSelection() {
+		// TODO Auto-generated method stub
+		spinner1 = (Spinner) findViewById(R.id.spinner1);
+		spinner1.setOnItemSelectedListener(new CustomOnItemSelectionListener());
+	}
+
+	public void addListenerOnButton(){
+		spinner1 = (Spinner) findViewById(R.id.spinner1);
+		
+		
+	}
+
 }
