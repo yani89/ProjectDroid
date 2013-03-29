a5e0b821bec79de70fe735ad14a843ce71634709
Harddisk-Yanipra <Harddisk-Yanipra@Harddisk-Yanipr>
2013-03-29 20:47:16 +0700

    Test lima mau koordinasi dengan database segera

--- ProjectDroid/src/com/projectdroid/app/OptionsActivity.java
+++ ProjectDroid/src/com/projectdroid/app/OptionsActivity.java
@@ -7,6 +7,7 @@ import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,30 +16,36 @@ public class OptionsActivity extends Activity {
    
 	private Spinner spinner1;
 	private Button btnSubmit;
+	private EditText edittext;
+	
 	
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settinglayout);
         
-        addListenerOnSpineerItemSelection();
+        //addListenerOnSpineerItemSelection();
+        addListenerOnButton();
     }
 
-	private void addListenerOnSpineerItemSelection() {
-		// TODO Auto-generated method stub
-		spinner1 = (Spinner) findViewById(R.id.spinner1);
-		spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
-	}
+//	private void addListenerOnSpineerItemSelection() {
+//		// TODO Auto-generated method stub
+//		spinner1 = (Spinner) findViewById(R.id.spinner1);
+//		spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+//	}
 
 	public void addListenerOnButton(){
 		spinner1 = (Spinner) findViewById(R.id.spinner1);
+		edittext = (EditText) findViewById(R.id.editText1);
 		btnSubmit = (Button) findViewById(R.id.btnSubmit);
+		
+		
 		
 		btnSubmit.setOnClickListener(new OnClickListener(){
 			@Override
 			public void onClick(View v){
 				Toast.makeText(OptionsActivity.this,
-				"OnclickListener : "+
-				"\nSpinner 1: " + String.valueOf(spinner1.getSelectedItem()), Toast.LENGTH_SHORT).show();
+				"\nSpinner 1: " + String.valueOf(spinner1.getSelectedItem())+ 
+				"\nSpinner 1: " + String.valueOf(edittext.getText()), Toast.LENGTH_SHORT).show();
 			}
 		});
 	}
