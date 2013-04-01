70c106403f2141629db7131899297978db7cd1dd
Yanipra <yanipra@gmail.com>
2013-04-02 00:08:37 +0700

    Soap in Action

--- ProjectDroid/src/com/projectdroid/app/OptionsActivity.java
+++ ProjectDroid/src/com/projectdroid/app/OptionsActivity.java
@@ -1,5 +1,11 @@
 package com.projectdroid.app;
 
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapPrimitive;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransportSE;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,6 +23,12 @@ public class OptionsActivity extends Activity {
 	private Spinner spinner1;
 	private Button btnSubmit;
 	private EditText edittext;
+	
+	/* soap */
+	private static String SOAP_ACTION1 = "DefaultNamespace";
+	private static String NAMESPACE = "";
+	private static String METHOD_NAME1="HelloWorld";
+	private static String URL = "http://localhost:8080/SOAPHelloWorld/HelloWorld?wsdl";
 	
 	
 	public void onCreate(Bundle savedInstanceState) {
@@ -43,6 +55,26 @@ public class OptionsActivity extends Activity {
 		btnSubmit.setOnClickListener(new OnClickListener(){
 			@Override
 			public void onClick(View v){
+				SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME1);
+				
+				request.addProperty("HelloWorld",edittext.getText().toString());
+				
+				SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+				
+				envelope.setOutputSoapObject(request);
+				envelope.dotNet = false;
+				
+				HttpTransportSE test = new HttpTransportSE(URL);
+				
+				try{
+					test.call(SOAP_ACTION1, envelope);
+					
+					SoapPrimitive sp = (SoapPrimitive) envelope.getResponse();
+					edittext.setText("Msg from service "+sp);
+				}catch(Exception e){
+					
+					e.printStackTrace();
+				}
 				Toast.makeText(OptionsActivity.this,
 				"\nSpinner 1: " + String.valueOf(spinner1.getSelectedItem())+ 
 				"\nSpinner 1: " + String.valueOf(edittext.getText()), Toast.LENGTH_SHORT).show();
