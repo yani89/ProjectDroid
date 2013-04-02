a0175f201eb57ff838a348d670d0a4cc0569807c
Yanipra <yanipra@gmail.com>
2013-04-03 00:14:26 +0700

    Belum bisa connect ke webservice ...coz pake SOAP androidny harus extends ke AsyncTask di android > 3.2

--- ProjectDroid/src/com/projectdroid/app/OptionsActivity.java
+++ ProjectDroid/src/com/projectdroid/app/OptionsActivity.java
@@ -7,6 +7,7 @@ import org.ksoap2.serialization.SoapSerializationEnvelope;
 import org.ksoap2.transport.HttpTransportSE;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -22,25 +23,12 @@ public class OptionsActivity extends Activity {
    
 	private Spinner spinner1;
 	private Button btnSubmit;
-	private EditText edittext;
+	private EditText edittext,edittext2,edittext3;
+	private TextView textView;
 	
-	/* soap */
-//	private static String SOAP_ACTION1 = "DefaultNamespace";
-//	private static String NAMESPACE = "http://webservice.javapapers.com/";
-//	private static String METHOD_NAME1="HelloWorld";
-//	private static String URL = "http://localhost:8080/SOAPHelloWorld/HelloWorld?wsdl";
-	
-	private static String SOAP_ACTION1 = "http://tempuri.org/FahrenheitToCelsius";
-
-    private static String SOAP_ACTION2 = "http://tempuri.org/CelsiusToFahrenheit";
-
-    private static String NAMESPACE = "http://tempuri.org/";
-
-    private static String METHOD_NAME1 = "FahrenheitToCelsius";
-
-    private static String METHOD_NAME2 = "CelsiusToFahrenheit";
-
-    private static String URL = "http://www.w3schools.com/webservices/tempconvert.asmx?WSDL";
+	private static final String MethodName = "hello";
+	private static final String Namespace = "http://ya/";
+	private static final String url = "http://192.168.1.114:8080/WebService/Test?wsdl";
 	
 	
 	public void onCreate(Bundle savedInstanceState) {
@@ -48,14 +36,46 @@ public class OptionsActivity extends Activity {
         setContentView(R.layout.settinglayout);
         
         //addListenerOnSpineerItemSelection();
-        addListenerOnButton();
-    }
+        //addListenerOnButton();
+        
+		spinner1 = (Spinner) findViewById(R.id.spinner1);
+		
+		textView = (TextView) findViewById(R.id.textView3);
 
-//	private void addListenerOnSpineerItemSelection() {
-//		// TODO Auto-generated method stub
-//		spinner1 = (Spinner) findViewById(R.id.spinner1);
-//		spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
-//	}
+		btnSubmit = (Button) findViewById(R.id.btnSubmit);
+		
+        
+   }
+
+	public class actionSoap extends AsyncTask<Void, Void,Void>
+	{
+		
+		@Override
+		protected Void doInBackground(Void... params) {
+			// TODO Auto-generated method stub
+			SoapObject soapOb = new SoapObject(OptionsActivity.Namespace, OptionsActivity.MethodName);
+			SoapSerializationEnvelope soapSerial = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+			soapSerial.setOutputSoapObject(soapOb);
+			
+			try
+			{
+				HttpTransportSE httpTran = new HttpTransportSE(OptionsActivity.url);
+				httpTran.debug = true;
+				httpTran.call(OptionsActivity.Namespace+ OptionsActivity.MethodName, soapSerial);
+				Object objectResult = (Object)soapSerial.getResponse();
+				textView.setText(objectResult.toString());
+				return null;
+				
+			}catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+			return null;
+		}
+		
+		
+	}
+	
 
 	public void addListenerOnButton(){
 		spinner1 = (Spinner) findViewById(R.id.spinner1);
@@ -67,66 +87,7 @@ public class OptionsActivity extends Activity {
 		btnSubmit.setOnClickListener(new OnClickListener(){
 			@Override
 			public void onClick(View v){
-				SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME1);       
-
-                
-
-                //Use this to add parameters
-
-                request.addProperty("Fahrenheit",edittext.getText().toString());
-
-               
-
-                //Declare the version of the SOAP request
-
-                SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
-
-               
-
-                envelope.setOutputSoapObject(request);
-
-                envelope.dotNet = true;
-				 try {
-
-                     HttpTransportSE androidHttpTransport = new HttpTransportSE(URL);
-
-                    
-
-                     //this is the actual part that will call the webservice
-
-                     androidHttpTransport.call(SOAP_ACTION1, envelope);
-
-                    
-
-                     // Get the SoapResult from the envelope body.
-
-                     SoapObject result = (SoapObject)envelope.bodyIn;
-
-
-
-                     if(result != null)
-
-                     {
-
-                           //Get the first property and change the label text
-
-                           edittext.setText(result.getProperty(0).toString());
-
-                     }
-
-                     else
-
-                     {
-
-                           Toast.makeText(getApplicationContext(), "No Response",Toast.LENGTH_LONG).show();
-
-                     }
-
-               } catch (Exception e) {
-
-                     e.printStackTrace();
-
-               }
+				
 				
 
 				Toast.makeText(OptionsActivity.this,
